e7d38a210b6e71d4131916f47933e8dd064a200b
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2015-06-14 18:27:08 +0200

    cleanup status bar

--- .config/i3/i3status.py
+++ .config/i3/i3status.py
@@ -48,19 +48,19 @@ def try_catch(func):
         pack(msg, COLOR_URGENT)
 
 
-def cpu():
-    cpus = [line.split() for line in run(CMD_CPU).split('\n')]
-    sep()
-    pack(ICON_CPU, COLOR_ICON)
-    for i in range(len(cpus)):
-        line = cpus[i]
-        perc = float(line[2].replace(',', '.'))
-        load = '{:5.2f}%'.format(perc)
-        load = '{:05.2f}%'.format(perc)
-        pack(load, COLOR_STD if perc <= 80.0 else COLOR_URGENT)
-        if i < len(cpus) - 1:
-            pack(' | ', COLOR_STD)
-
+# def cpu():
+#     cpus = [line.split() for line in run(CMD_CPU).split('\n')]
+#     sep()
+#     pack(ICON_CPU, COLOR_ICON)
+#     for i in range(len(cpus)):
+#         line = cpus[i]
+#         perc = float(line[2].replace(',', '.'))
+#         load = '{:5.2f}%'.format(perc)
+#         load = '{:05.2f}%'.format(perc)
+#         pack(load, COLOR_STD if perc <= 80.0 else COLOR_URGENT)
+#         if i < len(cpus) - 1:
+#             pack(' | ', COLOR_STD)
+ 
 
 def ram():
     line  = run(CMD_RAM).split()
@@ -83,7 +83,7 @@ def charge():
     perc_left = tokens[3] if len(tokens) == 4 else tokens[3][:-1]
     time_left = tokens[4] if len(tokens) != 4 else 'Full'
     txt_color = COLOR_URGENT if int(perc_left[:-1]) <= 25 else COLOR_STD
-    block(ICON_BATTERY if tokens[2] == 'Discharging,' else ICON_PLUG, perc_left, time_left, txt_color)
+    block(ICON_BATTERY if tokens[2] == 'Discharging,' else ICON_PLUG, perc_left + ' ' + time_left, txt_color)
 
 
 def date_time():
@@ -126,7 +126,7 @@ def main():
     stdout.write('[')
     stdout.write('[],')
     while True:
-        for func in [cpu, ram, online, charge, date_time, volume]:
+        for func in [ram, online, charge, date_time, volume]:
             try_catch(func)
         pack(' ', COLOR_SEPARATOR)
         stdout.write(dumps(BLOCKS) + ',')
