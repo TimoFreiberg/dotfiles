53cc3523f4550d65c4a5b14ee5cb0a0d7212f644
Timo Freiberg <timo.freiberg@gmail.com>
2015-06-23 20:14:25 +0200

    fix logic of padxormouse

--- bin/padXORmouse.sh
+++ bin/padXORmouse.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-if [ -n $(lsusb | ag mouse) ]; then
+if [ -n "$(lsusb | ag mouse)" ]; then
+  echo pad off
   /usr/bin/synclient TouchpadOff=1
 else
+  echo pad on
   /usr/bin/synclient TouchpadOff=0
 fi
