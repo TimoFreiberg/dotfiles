7237c09e3de1487e291174b737791f0d129b7322
Timo Freiberg <timo.freiberg@gmail.com>
2015-12-09 08:40:39 +0100

    mod PATH for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,6 @@
 # remove ansi term prompt issues
 # function fish_title;end
 
+set -gx PATH $HOME/bin $HOME/bin/nogit $HOME/.local/bin $HOME/.cabal/bin $PATH
+
 fish_vi_mode
--- .zshenv
+++ .zshenv
@@ -1,1 +1,1 @@
-export PATH=$HOME/bin:$HOME/.local/bin:$HOME/.cabal/bin:$PATH
+export PATH=$HOME/bin:$HOME/bin/nogit:$HOME/.local/bin:$HOME/.cabal/bin:$PATH
