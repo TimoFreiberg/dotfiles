0f7b142dd496c12d3b8457ce67df595711b41dd8
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2016-02-19 17:39:46 +0100

    spacemacs update in terminal

--- .config/fish/functions/spacemacs-update.fish
+++ .config/fish/functions/spacemacs-update.fish
@@ -1,6 +1,6 @@
 function spacemacs-update
 	cd ~/.emacs.d/
 git pull
-emacs --execute '(configuration-layer/update-packages t)' --execute '(kill-emacs)'
+emacs -nw --execute '(configuration-layer/update-packages t)' --execute '(kill-emacs)'
 cd -
 end
