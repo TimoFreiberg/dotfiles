44722e0b12059420592f02a42f849044af3ef681
Timo Freiberg <timo.freiberg@gmail.com>
2016-05-28 09:59:44 +0200

    only update lts version if changed, add git info to new prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,12 +2,49 @@ function fish_prompt
 	and set retc green; or set retc red
     tty|string match -q -r tty; and set tty tty; or set tty pts
 
+    function _git_branch_name
+        echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+    end
+    function _is_git_dirty
+        echo (git status -s --ignore-submodules=dirty ^/dev/null)
+    end
+    if [ (_git_branch_name) ]
+        set -l git_branch (set_color -o blue)(_git_branch_name)
+        if [ (_is_git_dirty) ]
+            for i in (git branch -qv --no-color| string match -r \*|cut -d' ' -f4-|cut -d] -f1|tr , \n)\
+                (git status --porcelain | cut -c 1-2 | uniq)
+                switch $i
+                    case "*[ahead *"
+                        set git_status "$git_status"(set_color red)⬆
+                    case "*behind *"
+                        set git_status "$git_status"(set_color red)⬇
+                    case "."
+                        set git_status "$git_status"(set_color green)✚
+                    case " D"
+                        set git_status "$git_status"(set_color red)✖
+                    case "*M*"
+                        set git_status "$git_status"(set_color green)✱
+                    case "*R*"
+                        set git_status "$git_status"(set_color purple)➜
+                    case "*U*"
+                        set git_status "$git_status"(set_color brown)═
+                    case "??"
+                        set git_status "$git_status"(set_color red)≠
+                end
+            end
+        else
+            set git_status (set_color green):
+        end
+        set git_info "git$git_status$git_branch"
+    end
+
     set_color $retc
     if [ $tty = tty ]
         echo -n .-
     else
         echo -n '┬─'
     end
+
     set_color -o green
     echo -n [
     if test $USER = root -o $USER = toor
@@ -29,6 +66,7 @@ function fish_prompt
     echo -n :(pwd|sed "s=$HOME=~=")
     set_color -o green
     echo -n ']'
+
     set_color normal
     set_color $retc
     if [ $tty = tty ]
@@ -36,6 +74,22 @@ function fish_prompt
     else
         echo -n '─'
     end
+
+    set_color -o green
+    echo -n [
+    set_color -o white
+    echo -n $git_info
+    set_color -o green
+    echo -n ']'
+
+    set_color normal
+    set_color $retc
+    if [ $tty = tty ]
+        echo -n '-'
+    else
+        echo -n '─'
+    end
+
     set_color -o green
     echo -n '['
     set_color normal
@@ -54,6 +108,7 @@ function fish_prompt
 		end
 	end
     echo
+
     set_color normal
     for job in (jobs)
         set_color $retc
@@ -65,6 +120,7 @@ function fish_prompt
         set_color brown
         echo $job
     end
+
     set_color normal
     set_color $retc
     if [ $tty = tty ]
--- .config/fish/functions/upgrayedd.fish
+++ .config/fish/functions/upgrayedd.fish
@@ -2,10 +2,17 @@ function upgrayedd
 	sudo pacman -Syu --ignore docker; and sudo aura -Au;
 	spacemacs-update; and spacemacs-update;
 	stack upgrade;
+	
 	set resolver (stack --resolver lts install ghc-mod hasktags hlint hindent stylish-haskell 2>&1 | tee (tty) | ag -o 'resolver:(.*)$')
+	
+	if test $resolver = (tail -n 1 $HOME/.stack/global-project/stack.yaml)
+		return 0
+	end
 	head -n -1 $HOME/.stack/global-project/stack.yaml > /tmp/stack-resolver
 	echo $resolver >> /tmp/stack-resolver
+
 	mv $HOME/.stack/global-project/stack.yaml $HOME/.stack/global-project/stack.yaml.backup
 	mv /tmp/stack-resolver $HOME/.stack/global-project/stack.yaml
+
 	echo "updated stack $resolver"
 end
