5d8fe4fac37133208ad7fc4f3c9c86d05e4fca9f
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2016-10-21 14:08:53 +0200

    remove spacemacs-update - do manually!

--- .config/fish/functions/spacemacs-update.fish
+++
@@ -1,7 +0,0 @@
-function spacemacs-update
-	cd ~/.emacs.d/
-git pull
-emacs -nw --execute '(configuration-layer/update-packages t)' --execute '(kill-emacs)'
-emacs -nw --execute '(kill-emacs)'
-cd -
-end
