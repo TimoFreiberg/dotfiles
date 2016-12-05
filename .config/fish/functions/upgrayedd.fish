772e692a3e53f75906fe3ee2429b0a3e05104ee8
Timo Freiberg <timo.freiberg@gmail.com>
2016-12-05 18:18:59 +0100

    switch to lts for haskell packages, only update resolver if variable is not null

--- .config/fish/functions/upgrayedd.fish
+++ .config/fish/functions/upgrayedd.fish
@@ -2,14 +2,16 @@ function upgrayedd
 	sudo pacman -Syu --ignore docker; and sudo aura -Au;
 	stack upgrade;
 	
-	set resolver (stack --resolver nightly install hasktags hlint hindent stylish-haskell hoogle 2>&1 | tee (tty) | ag -o 'resolver:(.*)$')
+	set resolver (stack --resolver lts install hasktags hlint hindent stylish-haskell hoogle 2>&1 | tee (tty) | ag -o 'resolver:(.*)$')
 	
 	if test $resolver = (tail -n 1 $HOME/.stack/global-project/stack.yaml)
 		echo "$resolver up to date"
 		return 0
 	end
-	head -n -1 $HOME/.stack/global-project/stack.yaml > /tmp/stack-resolver
-	echo $resolver >> /tmp/stack-resolver
+	if test -n $resolver
+		head -n -1 $HOME/.stack/global-project/stack.yaml > /tmp/stack-resolver
+		echo $resolver >> /tmp/stack-resolver
+	end
 
 	mv $HOME/.stack/global-project/stack.yaml $HOME/.stack/global-project/stack.yaml.backup
 	mv /tmp/stack-resolver $HOME/.stack/global-project/stack.yaml
