1cd929340d959b8ddee3c356ca3f3a6aafa2b1fe
Timo Freiberg <timo.freiberg@gmail.com>
2017-02-28 22:07:16 +0100

    Correct null-check for resolver in upgrayedd

--- .config/fish/functions/upgrayedd.fish
+++ .config/fish/functions/upgrayedd.fish
@@ -1,21 +1,23 @@
 function upgrayedd
 	pacaur -Syu --ignore docker
-	stack upgrade;
+  stack upgrade
 	
-	set resolver (stack --resolver lts install hasktags hlint hoogle 2>&1 | tee (tty) | ag -o 'resolver:(.*)$')
-	stack --resolver nightly install intero hindent
+  set resolver (stack --resolver lts install hasktags hlint hoogle 2>&1 | tee (tty) | ag -o 'resolver:(.*)$')
+  stack --resolver nightly install intero hindent
 	
-	if test $resolver = (tail -n 1 $HOME/.stack/global-project/stack.yaml)
-		echo "$resolver up to date"
-		return 0
-	end
-	if test -n $resolver
-		head -n -1 $HOME/.stack/global-project/stack.yaml > /tmp/stack-resolver
-		echo $resolver >> /tmp/stack-resolver
-	end
+	if test -n "$resolver"
+    if test "$resolver" = (tail -n 1 $HOME/.stack/global-project/stack.yaml)
+      echo "$resolver up to date"
+      return 0
+    else
+      head -n -1 $HOME/.stack/global-project/stack.yaml > /tmp/stack-resolver
+      echo $resolver >> /tmp/stack-resolver
+      mv $HOME/.stack/global-project/stack.yaml $HOME/.stack/global-project/stack.yaml.backup
+      mv /tmp/stack-resolver $HOME/.stack/global-project/stack.yaml
 
-	mv $HOME/.stack/global-project/stack.yaml $HOME/.stack/global-project/stack.yaml.backup
-	mv /tmp/stack-resolver $HOME/.stack/global-project/stack.yaml
-
-	echo "updated stack $resolver"
+      echo "updated stack $resolver"
+    end
+	else
+    echo "could not update stack resolver, doing nothing"
+  end
 end
