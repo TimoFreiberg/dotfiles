19113bc6a64e8a2152ded496993e246bdbd70ed6
Timo Freiberg <timo.freiberg@gmail.com>
2017-10-17 19:57:24 +0200

    Add --install-ghc to stack install commands in upgrayedd

--- .config/fish/functions/upgrayedd.fish
+++ .config/fish/functions/upgrayedd.fish
@@ -1,10 +1,10 @@
-# Defined in /tmp/fish.nyVr3F/upgrayedd.fish @ line 2
+# Defined in /tmp/fish.Beq7eT/upgrayedd.fish @ line 2
 function upgrayedd
 	pacaur -Syu --ignore docker
   stack upgrade
 	
-  set resolver (stack --resolver lts install hasktags hlint hoogle 2>&1 | tee (tty) | ag -o 'resolver:(.*)$')
-  stack --resolver nightly install hindent
+  set resolver (stack --resolver lts --install-ghc install hasktags hlint hoogle 2>&1 | tee (tty) | ag -o 'resolver:(.*)$')
+  stack --resolver nightly --install-ghc install hindent
 	
 	if test -n "$resolver"
     if test "$resolver" = (tail -n 1 $HOME/.stack/global-project/stack.yaml)
