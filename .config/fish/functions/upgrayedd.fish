b3743955831109a0f821d8feb223d3943c1cae70
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2017-12-04 08:53:17 +0100

    Remove haskell part from upgrayedd

--- .config/fish/functions/upgrayedd.fish
+++ .config/fish/functions/upgrayedd.fish
@@ -1,24 +1,27 @@
-# Defined in /tmp/fish.GM6sWc/upgrayedd.fish @ line 2
+# Defined in /tmp/fish.w1zK1j/upgrayedd.fish @ line 2
 function upgrayedd
 	pacaur -Syu
-  stack upgrade
-	
-  set resolver (stack --resolver lts --install-ghc install hasktags hlint hoogle 2>&1 | tee (tty) | ag -o 'resolver:(.*)$')
-  stack --resolver nightly --install-ghc install hindent
-	
-	if test -n "$resolver"
-    if test "$resolver" = (tail -n 1 $HOME/.stack/global-project/stack.yaml)
-      echo "$resolver up to date"
-      return 0
-    else
-      head -n -1 $HOME/.stack/global-project/stack.yaml > /tmp/stack-resolver
-      echo $resolver >> /tmp/stack-resolver
-      mv $HOME/.stack/global-project/stack.yaml $HOME/.stack/global-project/stack.yaml.backup
-      mv /tmp/stack-resolver $HOME/.stack/global-project/stack.yaml
 
-      echo "updated stack $resolver"
-    end
-	else
-    echo "could not update stack resolver, doing nothing"
-  end
+
+## haskell part
+#  stack upgrade
+#	
+#  set resolver (stack --resolver lts --install-ghc install hasktags hlint hoogle 2>&1 | tee (tty) | ag -o 'resolver:(.*)$')
+#  stack --resolver nightly --install-ghc install hindent
+#	
+#	if test -n "$resolver"
+#    if test "$resolver" = (tail -n 1 $HOME/.stack/global-project/stack.yaml)
+#      echo "$resolver up to date"
+#      return 0
+#    else
+#      head -n -1 $HOME/.stack/global-project/stack.yaml > /tmp/stack-resolver
+#      echo $resolver >> /tmp/stack-resolver
+#      mv $HOME/.stack/global-project/stack.yaml $HOME/.stack/global-project/stack.yaml.backup
+#      mv /tmp/stack-resolver $HOME/.stack/global-project/stack.yaml
+#
+#      echo "updated stack $resolver"
+#    end
+#	else
+#    echo "could not update stack resolver, doing nothing"
+#  end
 end
