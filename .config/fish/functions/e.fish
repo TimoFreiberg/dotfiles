90b623fcaa26eab6fe3e80b537ced61608cc3e2f
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2018-01-16 10:11:25 +0100

    Add -a to "e" fish alias

--- .config/fish/functions/e.fish
+++ .config/fish/functions/e.fish
@@ -1,3 +1,4 @@
-function e --description 'alias e=emacsclient -n -c'
-	emacsclient -n -c $argv;
+# Defined in /tmp/fish.Lvv7G7/e.fish @ line 2
+function e --description 'alias e=emacsclient -n -c -a'
+	emacsclient -n -c -a $argv;
 end
