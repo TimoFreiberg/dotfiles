411afdcb31bf7022f33aa5b0c4e616eec87fdd7c
Timo Freiberg <timo.freiberg@gmail.com>
2018-04-24 18:09:39 +0200

    Add ~/.gem/ruby/2.5.0/bin/ to fish PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-set -x PATH $PATH ~/bin ~/bin/nogit ~/.local/bin ~/.cabal/bin
+set -x PATH $PATH ~/bin ~/bin/nogit ~/.local/bin ~/.cabal/bin ~/.gem/ruby/2.5.0/bin/
 
 set -x EDITOR vim
 set -x VISUAL vim
