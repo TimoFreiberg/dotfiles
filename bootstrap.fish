6127aa3d1133b03d76145dcd6985e7c7b90c67a1
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2018-09-12 10:18:59 +0200

    Add bootstrap script

---
+++ bootstrap.fish
@@ -0,0 +1,30 @@
+#!/usr/bin/env fish
+
+set DOTFILEDIR (pwd)
+set HOMEDIR $HOME
+set tmpdir (mktemp -d)
+
+function backup_dotfile
+    set f $HOMEDIR/$argv
+    if test -e $f
+        echo "moving $f to $tmpdir/$argv"
+        mv $f $tmpdir/$argv
+    end
+end
+
+function symlink_dotfile
+    ln -s $DOTFILEDIR/$argv $HOMEDIR/$argv
+end
+
+set DOTFILES bin Wallpapers .config .ghci .gitconfig .gitignore .profile .spacemacs .tmux.conf
+
+for dotfile in $DOTFILES
+    backup_dotfile $dotfile
+    symlink_dotfile $dotfile
+end
+
+backup_dotfile .gitcredentials
+echo '[user]
+#name = $NAME
+#email = $EMAIL' > $HOMEDIR/.gitcredentials
+
