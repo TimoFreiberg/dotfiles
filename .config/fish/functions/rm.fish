e7f2885e59fc84c45bce9574e979e14758d2b56b
Timo Freiberg <timo.freiberg@gmail.com>
2018-09-14 09:05:48 +0200

    Add rm alias and t->trash abbr

--- .config/fish/functions/rm.fish
+++ .config/fish/functions/rm.fish
@@ -1,4 +1,4 @@
-# Defined in - @ line 0
-function rm --description 'alias rm=trash'
-	trash  $argv;
+# Defined in /tmp/fish.OUT69L/rm.fish @ line 2
+function rm
+	echo 'Use trash instead. If you really want to rm, use "rm".'; false
 end
