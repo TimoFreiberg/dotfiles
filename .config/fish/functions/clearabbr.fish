00987a3faf24536a325b9fa268bd00076d4a7495
Timo Freiberg <timo.freiberg@gmail.com>
2019-05-02 21:59:34 +0200

    Update abbr

--- .config/fish/functions/clearabbr.fish
+++ .config/fish/functions/clearabbr.fish
@@ -1,7 +1,8 @@
-# Defined in /tmp/fish.muqUHz/clearabbr.fish @ line 2
+# Defined in /tmp/fish.Uck8uB/clearabbr.fish @ line 2
 function clearabbr
 	for ab in (abbr)
-set aname (echo $ab | awk '{print $2}')
+# awk print $5 because `abbr` prints the abbreviation as 5th element
+set aname (echo $ab | awk '{print $5}')
 if echo $aname | grep -o "^'" > /dev/null
 set aname (echo $aname | tr -d "'")
 end
