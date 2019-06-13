684849af57442567f9d035a2b53b20080500749c
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2019-06-13 16:24:57 +0200

    Add KDE config

---
+++ .config/plasma-locale-settings.sh
@@ -0,0 +1,11 @@
+# Generated script, do not edit
+# Exports language-format specific env vars from startkde.
+# This script has been generated from kcmshell5 formats.
+# It will automatically be overwritten from there.
+export LANG=en_US.UTF-8
+export LC_NUMERIC=en_DE.UTF-8
+export LC_TIME=en_DE.UTF-8
+export LC_MONETARY=en_DE.UTF-8
+export LC_MEASUREMENT=en_DE.UTF-8
+export LC_COLLATE=en_DE.UTF-8
+export LANGUAGE=en_US:de
