e3964b71dbe641b0137bc8bb0580c527e554f7f2
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2019-06-14 14:17:35 +0200

    Remove locale customizations again

--- .config/plasma-locale-settings.sh
+++ .config/plasma-locale-settings.sh
@@ -3,9 +3,4 @@
 # This script has been generated from kcmshell5 formats.
 # It will automatically be overwritten from there.
 export LANG=en_US.UTF-8
-export LC_NUMERIC=de_DE.UTF-8
-export LC_TIME=de_DE.UTF-8
-export LC_MONETARY=de_DE.UTF-8
-export LC_MEASUREMENT=de_DE.UTF-8
-export LC_COLLATE=de_DE.UTF-8
 export LANGUAGE=en_US:de
