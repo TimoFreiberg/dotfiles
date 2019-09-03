6c282da63d72252ded31203cbd718e2997a4ed43
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2019-09-03 16:00:32 +0200

    Add starship call to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,10 @@ set -x PATH $PATH ~/bin ~/bin/nogit ~/.local/bin ~/.cabal/bin
 
 set -x EDITOR vim
 set -x VISUAL vim
+
+
+if which starship > /dev/null
+    eval (starship init fish)
+else
+    echo "Consider installing `starship`"
+end
