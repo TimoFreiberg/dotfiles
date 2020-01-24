5bc616a23d17e1788c905ceb85132014ff00b324
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2020-01-24 16:27:11 +0100

    Ignore dots in dirnames in tmux-new

--- .config/fish/functions/tmux-new.fish
+++ .config/fish/functions/tmux-new.fish
@@ -1,11 +1,11 @@
-# Defined in /tmp/fish.uf0Xkt/tmux-new.fish @ line 2
+# Defined in /tmp/fish.t6dvzo/tmux-new.fish @ line 2
 function tmux-new
 	if test -z $argv
         set dir (pwd)
     else
         set dir (realpath $argv)
     end
-    set sessname (basename "$dir")
+    set sessname (basename "$dir" | tr -d '.')
     if test -z $TMUX
         tmux new -c "$dir" -s "$sessname"
     else
