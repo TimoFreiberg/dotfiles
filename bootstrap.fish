a71a778ee6de1325fae78e4b548ab6f0664db9aa
Timo Freiberg <timo.freiberg@gmail.com>
2020-03-08 11:37:31 +0100

    Remove .gitignore from DOTFILES list in bootstrap.fish

--- bootstrap.fish
+++ bootstrap.fish
@@ -16,7 +16,7 @@ function symlink_dotfile
     ln -s $DOTFILEDIR/$argv $HOMEDIR/$argv
 end
 
-set DOTFILES bin Wallpapers .config .ghci .gitconfig .gitignore .profile .spacemacs .tmux.conf .kde4
+set DOTFILES bin Wallpapers .config .ghci .gitconfig .profile .spacemacs .tmux.conf 
 
 for dotfile in $DOTFILES
     backup_dotfile $dotfile
