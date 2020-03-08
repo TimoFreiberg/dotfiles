1261b0c82a4ccd8d88fbf0100e3a80e8cdd7ad4e
Timo Freiberg <timo.freiberg@gmail.com>
2020-03-08 17:54:11 +0100

    Add -A to `tmux new` command in tmux-new script

--- .config/fish/functions/tmux-new.fish
+++ .config/fish/functions/tmux-new.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.t6dvzo/tmux-new.fish @ line 2
+# Defined in /home/tf/.config/fish/functions/tmux-new.fish @ line 2
 function tmux-new
 	if test -z $argv
         set dir (pwd)
@@ -7,9 +7,9 @@ function tmux-new
     end
     set sessname (basename "$dir" | tr -d '.')
     if test -z $TMUX
-        tmux new -c "$dir" -s "$sessname"
+        tmux new -A -c "$dir" -s "$sessname"
     else
-        tmux new -c "$dir" -s "$sessname" -d
+        tmux new -A -c "$dir" -s "$sessname" -d
         tmux switch-client -t "$sessname"
     end
 end
