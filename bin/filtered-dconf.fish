b614c6dd7a631e943e804ef204fb33a1e6f3419c
Timo Freiberg <timo.freiberg@gmail.com>
2020-07-10 18:05:45 +0200

    Add scripts for managing dconf sync

---
+++ bin/filtered-dconf.fish
@@ -0,0 +1,43 @@
+#!/bin/env fish
+
+set TMP (mktemp)
+
+set IGNORED \
+    ca/desrt/dconf-editor \
+    com/github/wwmm/pulseeffects \
+    org/gnome/Characters \
+    org/gnome/Totem \
+    org/gnome/baobab/ui \
+    org/gnome/boxes \
+    org/gnome/builder \
+    org/gnome/calculator \
+    org/gnome/calendar \
+    org/gnome/cheese \
+    org/gnome/clocks \
+    org/gnome/control-center \
+    org/gnome/desktop/app-folders \
+    org/gnome/desktop/notifications \
+    org/gnome/eog/view \
+    org/gnome/evince/default \
+    org/gnome/evolution-data-server \
+    org/gnome/feedreader/saved-state \
+    org/gnome/file-roller \
+    org/gnome/gedit \
+    org/gnome/gnome-screenshot \
+    org/gnome/gnome-system-monitor \
+    org/gnome/meld/window-state \
+    org/gnome/nautilus \
+    org/gnome/software \
+    org/gtk/settings/color-chooser \
+    org/gtk/settings/file-chooser \
+    org/gnome/desktop/background \
+    org/gnome/desktop/screensaver
+
+dconf dump / > $TMP
+
+for IGNORE_PARAGRAPH in $IGNORED
+    set ESCAPED (echo $IGNORE_PARAGRAPH | regex-escape.py)
+    gawk -i inplace -v RS= -v ORS='\n\n' "!/$ESCAPED/" $TMP
+end
+
+bat $TMP
