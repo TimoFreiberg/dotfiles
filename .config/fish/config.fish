4163a186d211f80c480c1d95fd40ecce87302c3e
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2020-09-03 12:20:23 +0200

    Add gcp git cherry-pick abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -82,6 +82,7 @@ if status is-interactive
     abbr --add --global gpu 'git push --set-upstream origin HEAD'
     abbr --add --global gw 'git switch'
     abbr --add --global gwc 'git switch --create'
+    abbr --add --global gcp 'git cherry-pick'
 
     if command -v exa > /dev/null 
         abbr --add --global l exa
