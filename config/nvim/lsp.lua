a4e3171412ba0783120aeb124cc9d4d457b56098
Timo Freiberg <tfreiberg@fastly.com>
2022-12-13 08:08:53 +0100

    nvim: fix deprecated method usage in lsp.lua

--- config/nvim/lsp.lua
+++ config/nvim/lsp.lua
@@ -34,7 +34,7 @@ local lsp_flags = {
 }
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require('lspconfig')['rust_analyzer'].setup {
     on_attach = on_attach,
