ffd7fa389a9976eac705e5a48f71906060c7c3e9
Timo Freiberg-Schuberth <timo.freiberg@gmail.com>
2026-02-28 22:35:16 +0100

    Add --dangerously-skip-permissions to `cl` abbr

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -45,7 +45,7 @@ if status is-interactive
     abbr --add --global tn tmux-new
     abbr --add --global xb xsel -b
     abbr --add --global zi 'z -i'
-    abbr --add --global cl 'claude'
+    abbr --add --global cl 'claude --dangerously-skip-permissions'
     abbr --add --global wlp 'wl-paste'
     abbr --add --global wlc 'wl-copy'
 
