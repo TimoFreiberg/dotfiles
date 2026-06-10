127e7e0cac6ad7b1c0cb80ebc645ed9c75d798af
Timo Freiberg-Schuberth <tfreiberg@fastly.com>
2026-06-10 17:28:02 +0200

    alias sk="<run skepsis>"

---
+++ config/fish/functions/sk.fish
@@ -0,0 +1,3 @@
+function sk --wraps='npx --prefix ~/repos/skepsis tsx ~/repos/skepsis/cli.ts' --description 'alias sk=npx --prefix ~/repos/skepsis tsx ~/repos/skepsis/cli.ts'
+    npx --prefix ~/repos/skepsis tsx ~/repos/skepsis/cli.ts $argv
+end
