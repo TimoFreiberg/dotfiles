f972c3a1a4b19b547a6ef02c01cc5fcbc6eb9e73
Timo Freiberg-Schuberth <tfreiberg@fastly.com>
2026-06-10 17:30:05 +0200

    Fix sk alias path

--- config/fish/functions/sk.fish
+++ config/fish/functions/sk.fish
@@ -1,3 +1,3 @@
-function sk --wraps='npx --prefix ~/repos/skepsis tsx ~/repos/skepsis/cli.ts' --description 'alias sk=npx --prefix ~/repos/skepsis tsx ~/repos/skepsis/cli.ts'
-    npx --prefix ~/repos/skepsis tsx ~/repos/skepsis/cli.ts $argv
+function sk --wraps='npx --prefix ~/src/skepsis tsx ~/src/skepsis/cli.ts' --description 'alias sk=npx --prefix ~/src/skepsis tsx ~/src/skepsis/cli.ts'
+    npx --prefix ~/src/skepsis tsx ~/src/skepsis/cli.ts $argv
 end
